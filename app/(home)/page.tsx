0cab72ac6f6d2f660bbf6fcd070aa80de64b4fda
JavaPablo <pablopablo820@gmail.com>
2024-02-04 21:31:49 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
 
       <div className="px-5 pt-5">
+
         <h2 className="text-xl font-bold">Olá, Pablo Juan!</h2>
 
         <p className="capitalize text-sm">
@@ -16,11 +18,18 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+
       </div>
 
       <div className="px-5 mt-6">
         <Search />
       </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
+      </div>
+      
     </div>
   );
 }
